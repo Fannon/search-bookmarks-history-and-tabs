b7c26024494dbc003921a708fa127cca749c50a2
Simon Heimler <simon.heimler@sap.com>
2022-09-11 12:46:19 +0200

    Don't bundle external libraries into vendor.min.js anymore (#57)
    * Don't bundle external libraries into vendor.min.js anymore
    
    * Bump version to 1.6.2
    
    Co-authored-by: Simon Heimler <mail@simon-heimler.de>

--- bin/updateLibs.sh
+++ bin/updateLibs.sh
@@ -1,12 +1,8 @@
 rm -rf popup/lib
 mkdir popup/lib
 
-# Create minified vendor min.js where we bundle the search popup dependencies
-touch popup/lib/vendor.min.js
-# cat node_modules/fuse.js/dist/fuse.basic.min.js >> popup/lib/vendor.min.js
-cat node_modules/fuzzysort/fuzzysort.min.js >> popup/lib/vendor.min.js
-echo >> popup/lib/vendor.min.js
-cat node_modules/mark.js/dist/mark.es6.min.js >> popup/lib/vendor.min.js
+cat node_modules/fuzzysort/fuzzysort.min.js >> popup/lib/fuzzysort.min.js
+cat node_modules/mark.js/dist/mark.es6.min.js >> popup/lib/mark.es6.min.js
 
 # Copy over vendor dependencies that we don't bundle, because they're only loaded on demand
 cp node_modules/js-yaml/dist/js-yaml.min.js popup/lib/js-yaml.min.js
