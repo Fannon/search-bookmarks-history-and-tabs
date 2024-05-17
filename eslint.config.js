5c360de64d5cf2a17a734e3a73e0ed3f0ab2ce54
Simon Heimler <simon.heimler@sap.com>
2024-05-17 11:54:10 +0200

    Messed up eslint config..

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+const compat = new FlatCompat()
+
+export default [
+  js.configs.recommended,
+  ...compat.config({
+    extends: ['plugin:cypress/recommended'],
+  }),
+  {
+    files: ['popup/js/**/*.js'],
+    ignores: ['popup/lib/**.*', '/reports/*'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        'browser': true,
+        'es2021': true,
+        'webextensions': true,
+        'cypress/globals': true,
+        'commonjs': true,
+
+        'module': 'writable',
+        'ext': 'writable',
+        'Tagify': 'readonly',
+        'Mark': 'readonly',
+        'jsyaml': 'readonly',
+        'uFuzzy': 'readonly',
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'semi': ['warn', 'never'],
+      'comma-dangle': ['warn', 'only-multiline'],
+    },
+  },
+]
