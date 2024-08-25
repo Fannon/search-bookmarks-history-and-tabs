4c84deea4fc0a3fcb0681c0447a2e764e2e6459c
Simon Heimler <mail@simon-heimler.de>
2024-08-25 14:19:50 +0200

    Fix: Reset cache every hour needs own timer

--- popup/js/model/options.js
+++ popup/js/model/options.js
@@ -388,6 +388,7 @@ export async function setUserOptions(userOptions) {
 
     // Invalidate caches
     localStorage.removeItem('historyLastFetched')
+    localStorage.removeItem('historyLastReset')
     localStorage.removeItem('history')
 
     try {
