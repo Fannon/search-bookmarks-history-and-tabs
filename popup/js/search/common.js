29879150280b468f78d515b47f4409acdf79543f
Simon Heimler <mail@simon-heimler.de>
2025-06-02 21:01:12 +0200

    Add early return back, but add default entries
    Signed-off-by: Simon Heimler <mail@simon-heimler.de>

--- popup/js/search/common.js
+++ popup/js/search/common.js
@@ -40,6 +40,12 @@ export async function search(event) {
     searchTerm = searchTerm.trimStart().toLowerCase()
     searchTerm = searchTerm.replace(/ +(?= )/g, '') // Remove duplicate spaces
 
+    if (!searchTerm.trim()) {
+      ext.model.result = await addDefaultEntries()
+      renderSearchResults(ext.model.result)
+      return // Early return if no search term
+    }
+
     if (ext.opts.debug) {
       performance.mark('search-start')
     }
