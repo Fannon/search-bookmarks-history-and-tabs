70a0265a5bb1c9819a16ba69c09e0c602ea57fd1
Simon Heimler <mail@simon-heimler.de>
2025-10-05 12:07:24 +0200

    Exclude playwright tests from jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 export default {
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  testPathIgnorePatterns: ['<rootDir>/playwright/tests/'],
   moduleFileExtensions: ['js', 'json'],
-  transform: {}
+  transform: {},
 }
