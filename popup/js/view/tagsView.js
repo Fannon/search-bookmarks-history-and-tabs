a2bb7839cad32ecd421ef9e3ac924086d9d7effa
Simon Heimler <heimlersimon@gmail.com>
2026-01-03 10:40:05 +0100

    Add sorting of tags, folders and tab groups alphabetically vs. by count

--- popup/js/view/tagsView.js
+++ popup/js/view/tagsView.js
@@ -15,7 +15,21 @@ import { getUniqueTags } from '../search/taxonomySearch.js'
  */
 export function loadTagsOverview() {
   const tags = getUniqueTags()
-  const sortedTags = Object.keys(tags).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
+  const sortMode = localStorage.getItem('taxonomySortMode') || 'alpha'
+
+  let sortedTags = []
+  if (sortMode === 'count') {
+    sortedTags = Object.keys(tags).sort((a, b) => {
+      const countA = tags[a].length
+      const countB = tags[b].length
+      if (countA !== countB) {
+        return countB - countA // Higher count first
+      }
+      return a.localeCompare(b, undefined, { sensitivity: 'base' })
+    })
+  } else {
+    sortedTags = Object.keys(tags).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
+  }
 
   const container = document.getElementById('tags-list')
   if (!container) {
@@ -48,5 +62,39 @@ export function loadTagsOverview() {
     })
     .join('')
 
-  container.innerHTML = badgesHTML
+  // Add the list and a sort toggle button at the bottom
+  const sortLabel = sortMode === 'alpha' ? 'SORT BY COUNT' : 'SORT ALPHABETICALLY'
+  const newSortMode = sortMode === 'alpha' ? 'count' : 'alpha'
+
+  // Icon 1: Direction indicator (Descending for count, Ascending for alpha)
+  const sortDirectionIcon =
+    sortMode === 'alpha'
+      ? `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="icon-tabler-sort-descending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l9 0" /><path d="M4 10l7 0" /><path d="M4 14l3 0" /><path d="M4 18l3 0" /><path d="M17 15l3 3l3 -3" /><path d="M20 6l0 12" /></svg>`
+      : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="icon-tabler-sort-ascending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l7 0" /><path d="M4 10l7 0" /><path d="M4 14l3 0" /><path d="M17 9l3 -3l3 3" /><path d="M20 6l0 12" /></svg>`
+
+  // Icon 2: Type indicator (123 or ABC)
+  const sortTypeIcon =
+    sortMode === 'alpha'
+      ? `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="icon-tabler-123"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 10l2 -2v8" /><path d="M9 8h3a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 0 -1 1v2a1 1 0 0 0 1 1h3" /><path d="M17 8h2.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-1.5h1.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-2.5" /></svg>`
+      : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="icon-tabler-abc"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 16v-6a2 2 0 1 1 4 0v6" /><path d="M3 13h4" /><path d="M10 8v6a2 2 0 1 0 4 0v-1a2 2 0 1 0 -4 0v1" /><path d="M20.732 12a2 2 0 0 0 -3.732 1v1a2 2 0 0 0 3.726 1" /></svg>`
+
+  container.innerHTML = `
+    ${badgesHTML}
+    <div class="taxonomy-footer">
+      <button id="sort-toggle" class="button" data-sort="${newSortMode}" title="${sortLabel}">
+        ${sortDirectionIcon}
+        ${sortTypeIcon}
+        ${sortLabel}
+      </button>
+    </div>
+  `
+
+  // Add event listener for the sort toggle
+  const sortToggle = document.getElementById('sort-toggle')
+  if (sortToggle) {
+    sortToggle.addEventListener('click', () => {
+      localStorage.setItem('taxonomySortMode', sortToggle.dataset.sort)
+      loadTagsOverview()
+    })
+  }
 }
