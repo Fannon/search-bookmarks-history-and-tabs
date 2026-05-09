7ab0d68ae94d14d6c4d9ba3b1bd2e8b80c7172f1
Simon Heimler <heimlersimon@gmail.com>
2026-05-09 06:19:07 +0200

    Extract bookmark manager tag update planning

--- popup/js/initBookmarkManager.js
+++ popup/js/initBookmarkManager.js
@@ -38,6 +38,11 @@ import {
   uniqueTags,
 } from './model/bookmarkManagerOperations.js'
 import {
+  createBulkTagDescription,
+  createBulkTagMetadata,
+  createTagUpdatePlans,
+} from './model/bookmarkManagerTagUpdates.js'
+import {
   createBookmarkUndoSnapshot,
   createUndoHistoryExport,
   createUndoHistoryExportFilename,
@@ -1659,22 +1664,6 @@ async function updateTaggedBookmarks(bookmarks, getNextTags) {
   await updateTaggedBookmarkPlans(tagPlans)
 }
 
-function createTagUpdatePlans(bookmarks, getNextTags) {
-  const tagPlans = []
-
-  for (let i = 0; i < bookmarks.length; i++) {
-    const bookmark = bookmarks[i]
-    const currentTags = bookmark.tagsArray || []
-    const nextTags = getNextTags(currentTags)
-    if (areSameTags(currentTags, nextTags)) {
-      continue
-    }
-    tagPlans.push({ bookmark, currentTags, nextTags })
-  }
-
-  return tagPlans
-}
-
 async function updateTaggedBookmarkPlans(tagPlans) {
   for (let i = 0; i < tagPlans.length; i++) {
     const { bookmark, nextTags } = tagPlans[i]
@@ -1710,74 +1699,6 @@ function clearBulkTagInput() {
 
 function createDeletedDescription(bookmarkCount) {
   return `Deleted ${formatBookmarkCount(bookmarkCount)}`
-}
-
-function createBulkTagDescription(tagPlans) {
-  const addedTags = []
-  const removedTags = []
-
-  for (let i = 0; i < tagPlans.length; i++) {
-    const { currentTags, nextTags } = tagPlans[i]
-    appendTagDiff(addedTags, nextTags, currentTags)
-    appendTagDiff(removedTags, currentTags, nextTags)
-  }
-
-  const bookmarkText = formatBookmarkCount(tagPlans.length)
-  if (addedTags.length && removedTags.length) {
-    return `Changed tags on ${bookmarkText}: added "${addedTags.join(', ')}"; removed "${removedTags.join(', ')}"`
-  }
-  if (addedTags.length) {
-    return `Added tags "${addedTags.join(', ')}" to ${bookmarkText}`
-  }
-  if (removedTags.length) {
-    return `Removed tags "${removedTags.join(', ')}" from ${bookmarkText}`
-  }
-  return `Updated tags on ${bookmarkText}`
-}
-
-function createBulkTagMetadata(tagPlans) {
-  const addedTags = []
-  const removedTags = []
-
-  for (let i = 0; i < tagPlans.length; i++) {
-    const { currentTags, nextTags } = tagPlans[i]
-    appendTagDiff(addedTags, nextTags, currentTags)
-    appendTagDiff(removedTags, currentTags, nextTags)
-  }
-
-  return {
-    action: addedTags.length && removedTags.length ? 'updateTags' : addedTags.length ? 'addTags' : 'removeTags',
-    tagsAdded: addedTags,
-    tagsRemoved: removedTags,
-  }
-}
-
-function appendTagDiff(result, sourceTags, compareTags) {
-  const compareKeys = new Set(compareTags.map((tag) => tag.toLowerCase()))
-  const resultKeys = new Set(result.map((tag) => tag.toLowerCase()))
-
-  for (let i = 0; i < sourceTags.length; i++) {
-    const tag = sourceTags[i]
-    const key = tag.toLowerCase()
-    if (!compareKeys.has(key) && !resultKeys.has(key)) {
-      result.push(tag)
-      resultKeys.add(key)
-    }
-  }
-}
-
-function areSameTags(firstTags, secondTags) {
-  if (firstTags.length !== secondTags.length) {
-    return false
-  }
-
-  const firstKeys = new Set(firstTags.map((tag) => tag.toLowerCase()))
-  for (let i = 0; i < secondTags.length; i++) {
-    if (!firstKeys.has(secondTags[i].toLowerCase())) {
-      return false
-    }
-  }
-  return true
 }
 
 function createMoveDescription(bookmarks, parentId) {
