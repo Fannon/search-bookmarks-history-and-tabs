d4313b31af02546832fa93fc6666c013ceade469
Simon Heimler <heimlersimon@gmail.com>
2026-06-21 19:35:16 +0200

    Clone default search results (#356)
    * Clone default search results
    
    * Cover default result source mutation

--- popup/js/search/defaultResults.js
+++ popup/js/search/defaultResults.js
@@ -49,7 +49,7 @@ export async function addDefaultEntries() {
 
   if (ext.model.searchMode === 'history' && ext.model.history) {
     // Display recent history by default
-    results = ext.model.history
+    results = ext.model.history.map((el) => ({ ...el }))
   } else if (ext.model.searchMode === 'tabs' && ext.model.tabs) {
     // Display last opened tabs by default
     results = ext.model.tabs
@@ -59,7 +59,7 @@ export async function addDefaultEntries() {
       })
   } else if (ext.model.searchMode === 'bookmarks' && ext.model.bookmarks) {
     // Display all bookmarks by default
-    results = ext.model.bookmarks
+    results = ext.model.bookmarks.map((el) => ({ ...el }))
   } else {
     // Default: Find bookmarks that match current page URL
     let activeTab
@@ -70,7 +70,7 @@ export async function addDefaultEntries() {
         const currentUrl = cleanUpUrl(tab.url)
         const matchingBookmarks = ext.model.bookmarks.filter((el) => el.url === currentUrl)
         if (matchingBookmarks.length > 0) {
-          results.push(...matchingBookmarks)
+          results.push(...matchingBookmarks.map((el) => ({ ...el })))
         } else if (isQuickBookmarkEnabled() && isBookmarkableUrl(tab.url)) {
           results.push(createQuickBookmarkEntry(tab))
         }
